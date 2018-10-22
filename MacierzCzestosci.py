b054aaf64b01c87a19ae707221b8cf36fdde7f88
Patrycja Pawluk <patrycja.pawluk@connectpoint.local>
2018-10-22 19:50:57 +0200

    Przygotowanie macierzy częstości z wykorzystniem słowników języka polskiego oraz synonimów

---
+++ MacierzCzestosci.py
@@ -0,0 +1,45 @@
+import cx_Oracle
+import os
+
+os.environ["NLS_LANG"] = "POLISH_POLAND.AL32UTF8"
+connection = cx_Oracle.connect("C##PPAWLUK", "#Lex2018", "144.21.89.60:1521/ORCL.591263988.oraclecloud.internal")
+cur = connection.cursor()
+dokumentsName = cur.execute('select distinct nazwa_dokumentu from dokument')
+names = []
+for dokument in dokumentsName:
+    names.append(dokument[0])
+dokumentsText = []
+j = 0
+for name in names:
+    dokumentsText.append({'name': name, 'text': ''})
+    text = cur.execute('select tekst from dokument where nazwa_dokumentu = \'{}\''.format(name))
+    for t in text:
+        dokumentsText[j]['text'] = dokumentsText[j]['text'] + t[0]
+    j = j + 1
+allTekst = ''
+for z in dokumentsText:
+    allTekst = allTekst + z['text']
+allTekst = allTekst.split(' ')
+allTekst.remove('')
+allTekst = list(set(allTekst))
+matrix = []
+for word in allTekst:
+    for dokument in names:
+        matrix.append({"name":dokument,"word": word,'frequency':0})
+
+for dokument in dokumentsText:
+    print(dokument['text'])
+    text = dokument['text']
+    text = text.split(' ')
+    text.remove('')
+    for m in matrix:
+        if(m['name'] == dokument['name']):
+            m['frequency'] = text.count(m['word'])
+
+for m in matrix:
+    print(m)
+
+
+cur.execute('commit')
+cur.close()
+connection.close()
